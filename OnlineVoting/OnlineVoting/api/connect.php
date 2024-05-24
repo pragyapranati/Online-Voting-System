810cd28bfe4566b3c75f556d743f2535ef45d5c3
Pragya Pranati <128420734+pragyapranati@users.noreply.github.com>
2024-05-24 15:39:16 +0530

    Delete OnlineVoting/OnlineVoting directory

--- OnlineVoting/OnlineVoting/api/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "C:\Program Files\MySQL\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($con, "voting.mysql.database.azure.com", "username", "<password>", "voting", 3306, MYSQLI_CLIENT_SSL);
-    
-?>
